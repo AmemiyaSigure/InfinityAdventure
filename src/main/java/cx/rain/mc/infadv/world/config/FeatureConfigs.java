3612093a8906c7997454ed5f3f3f0d4631a45e5e
秋雨落 <i@rain.cx>
2021-02-09 12:13:33 +0800

    :sparkles: 临时提交
    Signed-off-by: 秋雨落 <i@rain.cx>

--- src/main/java/cx/rain/mc/infadv/world/config/FeatureConfigs.java
+++ src/main/java/cx/rain/mc/infadv/world/config/FeatureConfigs.java
@@ -4,5 +4,5 @@ import cx.rain.mc.infadv.block.ModBlocks;
 import net.minecraft.world.gen.feature.OreFeatureConfig;
 
 public class FeatureConfigs {
-    public static final OreFeatureConfig ORE_AQUAMARINE = new OreFeatureConfig(OreFeatureConfig.FillerBlockType.BASE_STONE_OVERWORLD, ModBlocks.AQUAMARINE_ORE.get().getDefaultState(), 3);
+    public static final OreFeatureConfig ORE_AQUAMARINE = new OreFeatureConfig(OreFeatureConfig.FillerBlockType.BASE_STONE_OVERWORLD, ModBlocks.AQUAMARINE_ORE.get().getDefaultState(), 6);
 }
